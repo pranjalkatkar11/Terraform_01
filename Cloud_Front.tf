3284f1f015e16d230616983cf0d62fe6782f0e85
pranjalkatkar11 <pranjalkatkar.117@gmail.com>
2025-01-17 00:10:58 +0530

    updated channges

--- Cloud_Front.tf
+++ Cloud_Front.tf
@@ -1,11 +1,14 @@
 resource "aws_cloudfront_distribution" "example" { 
+  # Origin configuration: Defines the source of your content (S3 bucket)
   origin { 
     domain_name = "my-bucket.s3.amazonaws.com" 
     origin_id   = "S3-my-bucket" 
   } 
  
+# Enable the distribution (whether it's active or not)
+enabled = true 
 
-enabled             = true 
+# Default Cache Behavior: Defines the caching behavior for CloudFront
   default_cache_behavior { 
     target_origin_id = "S3-my-bucket" 
     viewer_protocol_policy = "redirect-to-https" 
