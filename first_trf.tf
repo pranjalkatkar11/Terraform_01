c251f0e18bab56abd8734a0812dd2577a2eb39e6
pranjalkatkar11 <pranjalkatkar.117@gmail.com>
2025-01-16 20:02:24 +0530

    instance creation

---
+++ first_trf.tf
@@ -0,0 +1,11 @@
+provider "aws" { 
+  region = "ap-south-1" 
+} 
+
+resource "aws_instance" "example" { 
+  ami = "ami-053b12d3152c0cc71" 
+  instance_type = "t2.micro" 
+  tags = { 
+    Name = "ExampleInstance" 
+  } 
+}
